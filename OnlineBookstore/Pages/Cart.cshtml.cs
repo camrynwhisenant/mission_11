996249403b3281147aff08c3da7cec9983ece2d8
camrynwhisenant <cwhis99@byu.edu>
2022-02-23 13:43:44 -0700

    * Cart.cs: * Cart.cshtml: * Error.cshtml: * Index.cshtml: * Cart.cshtml.cs: * Privacy.cshtml: * Error.cshtml.cs: * Index.cshtml.cs: * Privacy.cshtml.cs: * _CartLayout.cshtml: * _Layout.cshtml: * UrlExtensions.cs: * SessionExtensions.cs: * ProjectSummary.cshtml: * _ValidationScriptsPartial.cshtml:
    * HomeController.cs: mission 8 commit
    
    * OnlineBookstore.csproj: mission 8 commit

---
+++ OnlineBookstore/Pages/Cart.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineBookstore.Infastructure;
+using OnlineBookstore.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace OnlineBookstore.Pages
+{
+    public class CartModel : PageModel
+    {
+        private IBookstoreRespository repo { get; set; }
+        public CartModel(IBookstoreRespository temp)
+        {
+            repo = temp;
+        }
+        public string ReturnUrl { get; set; }
+
+        public Cart cart { get; set; }
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+        public IActionResult OnPost(int bookId, string returnUrl)
+        {
+            Books b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
+            cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            cart.AddItem(b, 1);
+
+            HttpContext.Session.SetJson("cart", cart);
+            return RedirectToPage(new { ReturnUrl = returnUrl });
+
+        }
+    }
+}
